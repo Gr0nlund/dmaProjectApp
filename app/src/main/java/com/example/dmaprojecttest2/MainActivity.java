06e21b783d0b3c4af9de15b922d435cf40387cdc
Gr0nlund <72189884+Gr0nlund@users.noreply.github.com>
2021-03-29 15:27:53 +0200

    Set up main activity xml with framelayouts for fragments

--- app/src/main/java/com/example/dmaprojecttest2/MainActivity.java
+++ app/src/main/java/com/example/dmaprojecttest2/MainActivity.java
@@ -1,17 +1,11 @@
 package com.example.dmaprojecttest2;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-import com.example.dmaprojecttest2.Adapter.FragmentAdapter;
 import com.example.dmaprojecttest2.Fragments.MapsFragment;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+// Replace the contents of the container with the new fragment
+        ft.replace(R.id.frameLayout_bigMaps, new MapsFragment());
+// or ft.add(R.id.your_placeholder, new FooFragment());
+// Complete the changes added above
+        ft.commit();
+
+        /*FragmentTransaction ft2 = getSupportFragmentManager().beginTransaction();
+// Replace the contents of the container with the new fragment
+        ft2.replace(R.id.frameLayout_menu, new TestFragment());
+// or ft.add(R.id.your_placeholder, new FooFragment());
+// Complete the changes added above
+        ft2.commit();*/
 
     }
 }
