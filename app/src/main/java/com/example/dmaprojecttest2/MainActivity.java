a3f0b34a2b8fa8d400f929d590246bde2e984a3f
Gr0nlund <72189884+Gr0nlund@users.noreply.github.com>
2021-04-07 09:31:02 +0200

    recreate MapsFragment when menu is opened

--- app/src/main/java/com/example/dmaprojecttest2/MainActivity.java
+++ app/src/main/java/com/example/dmaprojecttest2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.dmaprojecttest2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -15,6 +16,8 @@ import com.example.dmaprojecttest2.Fragments.MenuFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    static MapsFragment mapsFragment = new MapsFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,38 +25,44 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.frameLayout_bigMaps,new MapsFragment())
+                .replace(R.id.frameLayout_bigMaps, mapsFragment)
                 .commitNow();
-
-
-        /*FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-// Replace the contents of the container with the new fragment
-        ft.replace(R.id.frameLayout_bigMaps, new MapsFragment());
-// or ft.add(R.id.your_placeholder, new FooFragment());
-// Complete the changes added above
-        ft.commit();*/
-
-
     }
 
     public static void createMenuFragment(View view) {
-
+        //creates the menu and places it in frameLayout
+        //creating the menuFragment should take parameters for the specific site
         FragmentActivity activity = (FragmentActivity)view.getContext();
         FragmentManager manager = activity.getSupportFragmentManager();
         FragmentTransaction ft2 = manager.beginTransaction();
-// Replace the contents of the container with the new fragment
+        // Replace the contents of the container with the new fragment
         ft2.replace(R.id.frameLayout_menu, MenuFragment.newInstance());
-// or ft.add(R.id.your_placeholder, new FooFragment());
-// Complete the changes added above
+        //completes transaction
         ft2.commit();
 
+        //moves mapsFragment from one frameLayout to another when it opens the menu
         FragmentTransaction ft = manager.beginTransaction();
-// Replace the contents of the container with the new fragment
-        ft.replace(R.id.frameLayout_smallMaps, new MapsFragment());
-// or ft.add(R.id.your_placeholder, new FooFragment());
-// Complete the changes added above
+        ft.remove(mapsFragment);
+        Fragment newInstance = recreateFragment(mapsFragment);
+        ft.add(R.id.frameLayout_smallMaps, newInstance);
         ft.commit();
     }
 
+    //adapted from https://stackoverflow.com/questions/9906254/illegalstateexception-cant-change-container-id-of-fragment
+    private static Fragment recreateFragment(Fragment f){
+        try {
+            FragmentActivity activity = (FragmentActivity) f.getContext();
+            FragmentManager fManager = activity.getSupportFragmentManager();
+            Fragment.SavedState savedState = fManager.saveFragmentInstanceState(f);
 
+            Fragment newInstance = f.getClass().newInstance();
+            newInstance.setInitialSavedState(savedState);
+
+            return newInstance;
+        }
+        catch (Exception e) // InstantiationException, IllegalAccessException
+        {
+            throw new RuntimeException("Cannot reinstantiate fragment " + f.getClass().getName(), e);
+        }
+    }
 }
